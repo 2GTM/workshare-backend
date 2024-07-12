6e97eb710d9eff1ca6ed296244aab7040373b3c5
AchrafGroiez <41470d0a@gmail.com>
2024-07-12 19:54:55 -0400

    Added LinkVisibility.

--- src/main/java/com/workshare/controller/ClientController.java
+++ src/main/java/com/workshare/controller/ClientController.java
@@ -1,11 +1,14 @@
 package com.workshare.controller;
 
+import com.workshare.repository.LinkRepository;
 import com.workshare.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 import java.util.Set;
 
 @CrossOrigin("${ALLOWED_URL}")
@@ -14,6 +17,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
+    private final LinkRepository linkRepository;
 
     @GetMapping("/usernames")
     public Set<String> getAllUsernames() {
