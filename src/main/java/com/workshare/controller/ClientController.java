5931f04b4c0252f5655aa48c0377129ce5f8a96c
AchrafGroiez <41470d0a@gmail.com>
2024-07-12 19:55:56 -0400

    Removed useless imports.

--- src/main/java/com/workshare/controller/ClientController.java
+++ src/main/java/com/workshare/controller/ClientController.java
@@ -1,14 +1,11 @@
 package com.workshare.controller;
 
-import com.workshare.repository.LinkRepository;
 import com.workshare.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 import java.util.Set;
 
 @CrossOrigin("${ALLOWED_URL}")
@@ -17,7 +14,6 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
-    private final LinkRepository linkRepository;
 
     @GetMapping("/usernames")
     public Set<String> getAllUsernames() {
