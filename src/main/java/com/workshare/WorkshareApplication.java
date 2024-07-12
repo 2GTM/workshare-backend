1e6e79f82a67c91b293ea4bb67a3afd34d960f7a
AchrafGroiez <41470d0a@gmail.com>
2024-07-12 11:44:09 -0400

    Added project.

---
+++ src/main/java/com/workshare/WorkshareApplication.java
@@ -0,0 +1,13 @@
+package com.workshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshareApplication.class, args);
+	}
+
+}
