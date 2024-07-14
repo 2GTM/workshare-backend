1ff25a8e38810bd8aaddb2c42767c76ba19e395e
guettafa <irontitou5@gmail.com>
2024-07-14 03:59:30 -0400

    Links Visibility is now Enumerated as String in LinkDto

--- src/main/java/com/workshare/dto/LinkDto.java
+++ src/main/java/com/workshare/dto/LinkDto.java
@@ -2,9 +2,14 @@ package com.workshare.dto;
 
 import com.workshare.enums.LinkVisibility;
 import com.workshare.model.Link;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 public record LinkDto(
+
+    @Enumerated(EnumType.STRING)
     LinkVisibility visibility,
+
     String content
 ) {
     public static LinkDto from(Link link) {
