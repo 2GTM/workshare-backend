b47fbbad9b26c7cfb3bf089a17e468e489f6c910
AchrafGroiez <41470d0a@gmail.com>
2024-07-14 06:17:37 -0400

    Removed JWT library in pom.xml

--- src/main/java/com/workshare/config/ApplicationConfig.java
+++ src/main/java/com/workshare/config/ApplicationConfig.java
@@ -1,17 +1,9 @@
 package com.workshare.config;
 
-import com.workshare.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
